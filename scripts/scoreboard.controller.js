8cc889604ba8a675fd34ca804fe85abc8745e177
Ryan Hoegg <ryan.hoegg@gmail.com>
2015-03-25 23:56:09 -0500

    Take private functions off  so they can not be used outside the controller

--- scripts/scoreboard.controller.js
+++ scripts/scoreboard.controller.js
@@ -16,16 +16,18 @@ angular.module("pixltalk2015").controller("ScoreboardController", function($scop
   		console.log("refreshing with " + url);
         $http.get(url)
           .success(function(data, status, headers, config) {
-          	$scope.updateScoreboard(angular.fromJson(data));
+          	updateScoreboard(angular.fromJson(data));
           });
   	};
 
-  	$scope.updateScoreboard = function(twitterResults) {
+  	function updateScoreboard(twitterResults) {
   		if (twitterResults.statuses.length > 0) {
   			console.log("processing " + twitterResults.search_metadata.count + " tweets, latest: " + twitterResults.statuses[0].text);
-          	$scope.scoreTweets(twitterResults.statuses);
-          	$scope.scoreboard.max_id = twitterResults.search_metadata.max_id;
-          	var context = { max: 0, winner: "rhoegg" };
+          	scoreTweets(twitterResults.statuses);
+          	
+            $scope.scoreboard.max_id = twitterResults.search_metadata.max_id;
+          	
+            var context = { max: 0, winner: "rhoegg" };
           	angular.forEach($scope.scoreboard.scores, function(score, user) {
           		if (score.tweets > this.max) {
           			this.max = score.tweets;
@@ -34,39 +36,39 @@ angular.module("pixltalk2015").controller("ScoreboardController", function($scop
           	}, context);
           	$scope.scoreboard.champion = "@" + context.winner;
   		}
-  	};
+  	}
 
-  	$scope.scoreTweets = function(tweets) {
-  		    var seekingFirst = true;
-          	angular.forEach(tweets, function(status, index) {
-          		if (status.id > $scope.scoreboard.max_id) {
-          			$scope.scoreboard.conferenceTweetCount += 1;
-                if ($scope.shouldCount(status.text)) {
-                      if (seekingFirst) { // first match is the most recent
-                        seekingFirst = false;
-                          $scope.scoreboard.newestTweet = status.text;
-                        $scope.scoreboard.challenger = "@" + status.user.screen_name;
-                      }
-                      $scope.scoreboard.angularTweetCount += 1;
-                    var score = $scope.scoreboard.scores[status.user.screen_name] || {
-                      tweets: 0
-                    };
-                    score.tweets = score.tweets + 1;
-                    $scope.scoreboard.scores[status.user.screen_name] = score;
+  	function scoreTweets(tweets) {
+	    var seekingFirst = true;
+    	angular.forEach(tweets, function(status, index) {
+    		if (status.id > $scope.scoreboard.max_id) {
+    			$scope.scoreboard.conferenceTweetCount += 1;
+          if (shouldCount(status.text)) {
+                if (seekingFirst) { // first match is the most recent
+                  seekingFirst = false;
+                    $scope.scoreboard.newestTweet = status.text;
+                  $scope.scoreboard.challenger = "@" + status.user.screen_name;
                 }
-          		} else {
-          			console.log("funny, got an old one: " + status.text);
-          		}
-          	});
-          	seekingFirst = true;
-  	};
+                $scope.scoreboard.angularTweetCount += 1;
+              var score = $scope.scoreboard.scores[status.user.screen_name] || {
+                tweets: 0
+              };
+              score.tweets = score.tweets + 1;
+              $scope.scoreboard.scores[status.user.screen_name] = score;
+          }
+    		} else {
+    			console.log("funny, got an old one: " + status.text);
+    		}
+    	});
+    	seekingFirst = true;
+  	}
 
-  	$scope.shouldCount = function(tweetText) {
+  	function shouldCount(tweetText) {
   		function escapeRegExp(expression) {
   			return expression.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
   		}
-		return new RegExp(escapeRegExp($scope.scoreboard.whatToMatch)).test(tweetText);
-  	};
+		  return new RegExp(escapeRegExp($scope.scoreboard.whatToMatch)).test(tweetText);
+  	}
 
   	$http.get("http://cors-anywhere.herokuapp.com/http://gentle-beyond-4243.herokuapp.com/1.1/search/tweets.json?q=%23" + baseSearchTerm + "&count=3&include_entities=0")
   		.success(function(data, status, headers, config) {
